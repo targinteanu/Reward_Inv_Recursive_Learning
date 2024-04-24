8299b98217c44b373816e987fbbc46d20ab3fe30
Toren Arginteanu <targinteanu@outlook.com>
2024-04-24 03:50:42 -0400

    updated figure

--- getStateSpace.m
+++ getStateSpace.m
@@ -123,27 +123,27 @@ if depict
     taskcondtype = {'choice', 'forced'};
     figure('Units', 'normalized', 'Position', [.1,.1,.5,.5]);
     %%{
-    plot(tgt_px_trl, tgt_py_trl, 'b-o')
+    plot(tgt_px_fx, tgt_py_fx, 'bx', 'MarkerSize',10,'LineWidth',2)
     hold on;
-    plot(tgt_px_trl(1), tgt_py_trl(1), 'bx', 'MarkerSize',10);
-    plot(tgt_px_trl(end), tgt_py_trl(end), 'b+', 'MarkerSize',10);
+    plot(tgt_px_lo, tgt_py_lo, 'bv', 'MarkerSize',10,'LineWidth',2);
+    plot(tgt_px_hi, tgt_py_hi, 'b^', 'MarkerSize',10,'LineWidth',2);
     %}
     plot(eye_px_filt_trl, eye_py_filt_trl, 'r');
     hold on;
     plot(eye_px_filt_trl(1), eye_py_filt_trl(1), 'rd', 'MarkerSize',8);
     plot(eye_px_filt_trl(end), eye_py_filt_trl(end), 'rs', 'MarkerSize',8);
-    plot(dta.cue_x_high_rew(trl), dta.cue_y_high_rew(trl), '^k', 'MarkerSize',10,'LineWidth',3);
-    plot(dta.cue_x_low_rew(trl),  dta.cue_y_low_rew(trl),  'vk', 'MarkerSize',10,'LineWidth',3);
-    plot(dta.cue_x(trl),  dta.cue_y(trl),  'ok', 'MarkerSize',10,'LineWidth',2);
-    plot(dta.start_x(trl), dta.start_y(trl), 'xk', 'MarkerSize',8,'LineWidth',2);
-    plot(dta.end_x(trl), dta.end_y(trl), '+k', 'MarkerSize',8,'LineWidth',2);
+    %plot(dta.cue_x_high_rew(trl), dta.cue_y_high_rew(trl), '^k', 'MarkerSize',10,'LineWidth',3);
+    %plot(dta.cue_x_low_rew(trl),  dta.cue_y_low_rew(trl),  'vk', 'MarkerSize',10,'LineWidth',3);
+    %plot(dta.cue_x(trl),  dta.cue_y(trl),  'ok', 'MarkerSize',10,'LineWidth',2);
+    %plot(dta.start_x(trl), dta.start_y(trl), 'xk', 'MarkerSize',8,'LineWidth',2);
+    %plot(dta.end_x(trl), dta.end_y(trl), '+k', 'MarkerSize',8,'LineWidth',2);
     plot(eye_px_filt_trl(newState), eye_py_filt_trl(newState), 'or', 'MarkerSize',10);
     grid on;
     legend(...
-        'tgt p', 'tgt start', 'tgt end', ...
-        'eye p filt', 'eye start', 'eye end', ...
-        'cue hi', 'cue lo', 'cue', 'start', 'end', ...
-        'cue moved', ...
+        'fix tgt', 'lo tgt', 'hi tgt', ...
+        'eye p', 'eye start', 'eye end', ...
+        ...'cue hi', 'cue lo', 'cue', 'start', 'end', ...
+        'tgt moved', ...
         'Location', 'eastoutside');
     ttl = [taskcondtype{dta.task_cond(trl)+1},' trial; '];
     if dta.task_cond(trl)
@@ -159,10 +159,13 @@ if depict
     title(ttl);
 
     figure('Units', 'normalized', 'Position', [.5,.5,.4,.4]);
-    plot(t, tgt_px_trl, '-om'); hold on; plot(t, tgt_py_trl, '-og');
-    plot(t, eye_px_filt_trl, 'm'); plot(t, eye_py_filt_trl, 'g');
+    plot(t, tgt_px_fx, '--r'); hold on; plot(t, tgt_py_fx, '--b');
+    plot(t, tgt_px_hi,  '-r'); hold on; plot(t, tgt_py_hi,  '-b');
+    plot(t, tgt_px_lo,  ':r'); hold on; plot(t, tgt_py_lo,  ':b');
+    plot(t, eye_px_filt_trl, 'Color', [1,.5,.5]); plot(t, eye_py_filt_trl, 'Color', [.5,.5,1]);
     grid on;
-    legend('tgt x', 'tgt y', 'eye x', 'eye y', 'Location','eastoutside');
+    legend('tgt x fix', 'tgt y fix', 'tgt x hi', 'tgt y hi', 'tgt x lo', 'tgt y lo', ...
+        'eye x', 'eye y', 'Location','eastoutside');
     xlabel('time (s)'); ylabel('pos');
     title(ttl);
 end
