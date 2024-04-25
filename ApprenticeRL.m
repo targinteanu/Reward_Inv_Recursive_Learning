a6060d7ed5cd380acb6fd8ac6aa2f7bea02ed777
Toren Arginteanu <targinteanu@outlook.com>
2024-04-25 03:26:21 -0400

    runs through muE step as grid world

--- ApprenticeRL.m
+++ ApprenticeRL.m
@@ -1,6 +1,8 @@
 gamma = .5; % discount factor 
+%{
 phi = @(s) 1./(1 + exp(-s/20)); % sigmoid mapping to [0,1]
 inv_phi = @(ph) 20*log(ph./(1-ph)); % inv sigmoid mapping to [-inf, inf]
+%}
 
 % loop through all 
 ind1 = randi(length(data_all_trials)); 
@@ -16,13 +18,17 @@ ind1 = randi(length(data_all_trials));
         Srec = []; Arec = []; 
         for trl = 1:(length(dta.task_cond))
             [Strl, Atrl] = getStateSpace(dta,trl,false);
+            Strl = gridifyState(Strl); Atrl = gridifyState(Atrl);
             Srec = [Srec; Strl]; 
             Arec = [Arec; Atrl];
 
-            Phi = phi(Strl{1:height(Strl), 1:width(Strl)}');
-            Gamma = gamma.^(0:(height(Strl)-1));
-%            muE = muE + Phi*Gamma'; 
-            muE(:,ind2) = muE(:,ind2) + Phi*Gamma';
+            if ~isempty(Strl)
+                % Phi = phi(Strl{1:height(Strl), 1:width(Strl)}');
+                Phi = phiGrid(Strl); Phi = Phi{:,:}';
+                Gamma = gamma.^(0:(height(Strl)-1));
+%               muE = muE + Phi*Gamma';
+                muE(:,ind2) = muE(:,ind2) + Phi*Gamma';
+            end
         end
 
         Srec.Time = Srec.Time - Srec.Time(1);
@@ -36,9 +42,10 @@ ind1 = randi(length(data_all_trials));
         Sapp = Srec(randi(height(Srec)),:); Strl = Sapp; 
         Aapp = []; Atrl = Arec(1,:);
         for trl = 1:height(Srec)
-            Atrl.eye_px_filt_trl = inv_phi(rand); 
-            Atrl.eye_py_filt_trl = inv_phi(rand);
-            Strl = updateState(Strl, Atrl); 
+            Atrl.eye_px_filt_trl = rand; 
+            Atrl.eye_py_filt_trl = rand;
+            Atrl = phiGridInv(Atrl);
+            Strl = updateGridState(Strl, Atrl); 
             Sapp = [Sapp; Strl]; Aapp = [Aapp; Atrl];
         end
         Phi = phi(Sapp{1:height(Sapp), 1:width(Sapp)}');
