6fea4a45ce87f557b162cc7db75e9311794a36fe
Toren Arginteanu <targinteanu@outlook.com>
2024-05-05 11:38:34 -0400

    working on interpreting/evaluating Q learned

---
+++ pickBestAct.m
@@ -0,0 +1,23 @@
+function [A, Q] = pickBestAct(S, Qtbl)
+
+stateSpace = Qtbl(:,1); actSpace = Qtbl(1,:); 
+stateSpace = stateSpace(2:end); actSpace = actSpace(2:end); 
+Q = cell2mat(Qtbl(2:end,2:end));
+
+r = 0;
+stateFound = false; 
+while ~stateFound & (r < length(stateSpace))
+    r = r+1;
+    stateFound = isEqualState(S, stateSpace{r});
+end
+
+if ~stateFound 
+    A = actSpace{1}; % move by [0, 0] 
+    Q = [];
+else
+    Q = Q(r,:);
+    [Q,c] = max(Q); 
+    A = actSpace{c};
+end
+
+end
