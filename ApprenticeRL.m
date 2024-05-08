6dc4b46fcac725c0762c1e4c8c10dfb8e89b72d5
Toren Arginteanu <targinteanu@outlook.com>
2024-05-08 10:50:45 -0400

    testing and training accuracy of policy

--- ApprenticeRL.m
+++ ApprenticeRL.m
@@ -18,7 +18,7 @@ muE = zeros(8, length(dtas));
 for ind2 = 1:(length(dtas))
     dta = dtas(ind2);
     disp(['Analyzing ',dta.name,' ...']);
-    Srec = []; Arec = []; 
+    Srec = []; Arec = []; % recorded state/action 
     trlct = 0;
     for trl = 1:(length(dta.task_cond))
         [Strl, Atrl] = getStateSpace(dta,trl,false);
@@ -48,8 +48,8 @@ clear dta trlct Phi Gamma
 disp('Initializing Apprentice...')
 mu0 = zeros(8, length(dtas));
 for ind2 = 1:(length(dtas))
-    Sapp = Srec(randi(height(Srec)),:); Strl = Sapp; 
-    Aapp = []; Atrl = Arec(1,:);
+    Sapp = Srec(randi(height(Srec)),:); Strl = Sapp; % apprentice state
+    Aapp = []; Atrl = Arec(1,:); % apprentice action 
     for trl = 1:min(height(Srec), 100)
         Atrl.eye_px_filt_trl = rand; 
         Atrl.eye_py_filt_trl = rand;
@@ -134,7 +134,7 @@ clear eyeX eyeY fixX fixY loX loY hiX hiY A1 S1
 disp('State-Action space initialized.')
 
 %% iterate until policy convergence 
-ind3 = 1; Del = inf; theta = .001; 
+Del = inf; theta = .001; 
 MT = [muE, mu0]; YT = [1, 0]; 
 Qtbls = []; Dels = [Del];
 
@@ -164,3 +164,61 @@ while Del > theta
     MT = [MT, Phi*Gamma']; YT = [YT, 0];
 
 end
+
+%% pick best of tested policies 
+m = quadprog(eye(length(muE)), 0*muE, eye(length(muE)), muE);
+mu2 = muE - m;
+M = MT(:,2:end);
+lambda = quadprog(M'*M, M'*muE, -eye(size(M,2)), zeros(size(M,2),1), ones(1,size(M,2)), 1);
+mu = MT(:,2:end)*lambda;
+Qtbl2 = zeros(size(Qtbl2));
+for ind3 = 1:size(Qtbls,3)
+    Qtbl2 = Qtbl2 + lambda(ind3)*Qtbls(:,:,ind3);
+end
+
+%% pick best action at each state under this policy 
+Abest = zeros(size(Qstate)); 
+for ind3 = 1:length(Abest)
+    [~,Abest(ind3)] = max(Qtbl2(ind3,:));
+end
+
+%% test accuracy of this policy against training data 
+disp('Computing training accuracy...')
+train_result = false(height(Srec),1);
+for t = 1:height(Srec)
+    St = Srec(t,:); At = Arec(t,:);
+    StInd = state2ind(St,L); 
+    if ~isEqualState(St, Qstate{StInd})
+        error('State does not match index of table.')
+    end
+    Apred = Qaction{Abest(StInd)};
+    train_result(t) = isEqualState(Apred, At);
+end
+training_accuracy = mean(train_result)
+
+%% test accuracy of this policy against other data
+disp('Computing testing accuracy...')
+indTest = randi(length(data_all_trials)); 
+dtasTest = data_all_trials{ind1};
+test_result = logical([]);
+for ind2 = 1:(length(dtasTest))
+    dta = dtasTest(ind2);
+    disp(['Testing ',dta.name,' ...']);
+    for trl = 1:(length(dta.task_cond))
+        [Strl, Atrl] = getStateSpace(dta,trl,false);
+        Strl = gridifyState(Strl); Atrl = gridifyState(Atrl);
+        if ~isempty(Strl)
+            for t = 1:height(Strl)
+                St = Strl(t,:); At = Atrl(t,:);
+                StInd = state2ind(St,L);
+                if ~isEqualState(St, Qstate{StInd})
+                    error('State does not match index of table.')
+                end
+                Apred = Qaction{Abest(StInd)};
+                test_result = [test_result; isEqualState(Apred, At)];
+            end
+        end
+    end
+end
+testing_accuracy = mean(test_result)
+clear dta 
